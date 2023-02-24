b1988e916b76edbb55493298d793a15fda61cb8a
HenriqueNas <brkshen@gmail.com>
2023-02-24 15:39:25 -0300

    refactor: update example files
    update usage of Wed package at /web/main.dart and /index.html

--- web/main.dart
+++ web/main.dart
@@ -5,50 +5,52 @@ import 'package:wed/wed.dart';
 final app = querySelector("#output") as DivElement;
 
 void main() {
-  final button = Button(
-    props: ButtonProps(
-      text: 'Click me',
-      onClick: () => print('Clicked'),
-      styles: CssBaseStyle(
-        height: '50px',
-        width: '150px',
-        textAlign: 'center',
-        color: 'white',
-        backgroundColor: 'purple',
+  renderApp(MyComponent(), app);
+}
+
+class MyComponent extends Component {
+  var text = 'Hello World';
+  var isClicked = false;
+
+  void toggleText() {
+    isClicked = !isClicked;
+    text = isClicked ? 'Clicked' : 'Hello World';
+  }
+
+  @override
+  List<Component> build() {
+    return [
+      Div(
+        props: DivProps(
+          styles: CssStyle(
+            width: Units.vw(100),
+            height: Units.vh(100),
+            backgroundColor: 'lightblue',
+          ).merge(DisplayFlex.center),
+        ),
+        children: [
+          Button(
+            props: ButtonProps(
+              innerText: text,
+              styles: CssStyle(
+                backgroundColor: 'blue',
+                fontSize: Units.rem(2),
+                color: 'white',
+                textAlign: TextAlign.center,
+                padding: Padding.symmetric(horizontal: 30, vertical: 20),
+                borderRadius: BorderRadius.all(12),
+                borderWidth: Units.none,
+                cursor: Cursor.pointer,
+              ),
+              onClick: (_) {
+                setState(() {
+                  toggleText();
+                });
+              },
+            ),
+          ),
+        ],
       ),
-    ),
-  );
-
-  final div = Div(
-    props: DivProps(
-      children: [button],
-      styles: CssBaseStyle(
-        height: '300px',
-        width: '300px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white',
-        borderRadius: BorderRadius.all(50),
-      ),
-    ),
-  );
-
-  const componentStyle = CssBaseStyle(
-    height: '100vh',
-    width: '100vw',
-    backgroundColor: 'blue',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  );
-
-  final component = Div(
-    props: DivProps(
-      styles: componentStyle,
-      children: [div],
-    ),
-  );
-
-  renderApp(component, app);
+    ];
+  }
 }
