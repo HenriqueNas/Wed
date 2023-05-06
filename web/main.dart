de80a669e31b6165e05766a565d10391ce615166
HenriqueNas <henriquenas.dev@gmail.com>
2023-05-06 11:40:21 -0300

    fix: :ambulance: fix usages of wed Component

--- web/main.dart
+++ web/main.dart
@@ -5,34 +5,36 @@ import 'package:wed/wed.dart';
 final app = querySelector('#output') as DivElement;
 
 void main() {
-  renderWidget(TestComponent(), app);
+  renderApp(TestComponent(), app);
 }
 
-class TestComponent extends Widget {
+class TestComponent extends Component {
   @override
-  Widget build() {
-    return Div(
-      props: const DivProps(
-        styles: CssStyle(
-          backgroundColor: 'red',
-          width: '100px',
-          height: '100px',
-        ),
-      ),
-      children: [
-        Button(
-          props: ButtonProps(
-            onClick: (_) => print('clicked'),
-            innerText: 'Click me',
-            disabled: false,
-            styles: const CssStyle(
-              backgroundColor: 'blue',
-              width: '50px',
-              height: '50px',
-            ),
+  List<Component> build() {
+    return [
+      Div(
+        props: const DivProps(
+          styles: CssStyle(
+            backgroundColor: 'red',
+            width: '100px',
+            height: '100px',
           ),
-        )
-      ],
-    );
+        ),
+        children: [
+          Button(
+            props: ButtonProps(
+              onClick: (_) => print('clicked'),
+              innerText: 'Click me',
+              disabled: false,
+              styles: const CssStyle(
+                backgroundColor: 'blue',
+                width: '50px',
+                height: '50px',
+              ),
+            ),
+          )
+        ],
+      ),
+    ];
   }
 }
--- web/my_component.dart
+++ web/my_component.dart
@@ -1,155 +1,133 @@
 import 'package:wed/wed.dart';
 
-class MyWidget extends Widget {
-  var text = 'Hello World';
-  var isClicked = false;
-
-  void toggleText() {
-    isClicked = !isClicked;
-    text = isClicked ? 'Clicked' : 'Hello World';
-  }
-
+class TopBar extends Component {
   @override
-  Widget build() {
-    return Div(
-      props: DivProps(
-        styles: CssStyle(
-          width: Units.vw(100),
-          height: Units.vh(100),
-          backgroundColor: 'white',
+  List<Component> build() {
+    return [
+      Div(
+        props: DivProps(
+          styles: CssStyle(
+            width: Units.vw(100),
+            height: Units.vh(10),
+            backgroundColor: '#2196F3',
+          ).merge(DisplayFlex.center),
         ),
-      ),
-      children: [
-        TopBar(),
-        Body(),
-      ],
-    );
-  }
-}
-
-class TopBar extends Widget {
-  @override
-  Widget build() {
-    return Div(
-      props: DivProps(
-        styles: CssStyle(
-          width: Units.vw(100),
-          height: Units.vh(10),
-          backgroundColor: '#2196F3',
-        ).merge(DisplayFlex.center),
-      ),
-      children: [
-        Span(
-          'Welcome to WED!',
-          props: SpanProps(
-            styles: CssStyle(
-              color: 'white',
-              fontSize: Units.rem(2),
+        children: [
+          Span(
+            'Welcome to WED!',
+            props: SpanProps(
+              styles: CssStyle(
+                color: 'white',
+                fontSize: Units.rem(2),
+              ),
             ),
           ),
-        ),
-      ],
-    );
+        ],
+      ),
+    ];
   }
 }
 
-class Body extends Widget {
+class Body extends Component {
   double _count = 0;
 
   void _increment() {
-    print('Incremented');
-
     setState(() {
       _count++;
-      print(_count);
     });
   }
 
   @override
-  Widget build() {
-    return Div(
-      props: DivProps(
-        styles: CssStyle(
-          width: Units.vw(100),
-          height: Units.vh(90),
-          backgroundColor: '#F5F5F5',
-          display: Display.flex,
-          flexDirection: FlexDirection.column,
-          alignItems: AlignItems.flexEnd,
-          justifyContent: JustifyContent.spaceBetween,
-          boxSizing: 'border-box',
+  List<Component> build() {
+    return [
+      Div(
+        props: DivProps(
+          styles: CssStyle(
+            width: Units.vw(100),
+            height: Units.vh(90),
+            backgroundColor: '#F5F5F5',
+            display: Display.flex,
+            flexDirection: FlexDirection.column,
+            alignItems: AlignItems.flexEnd,
+            justifyContent: JustifyContent.spaceBetween,
+            boxSizing: 'border-box',
+          ),
         ),
+        children: [
+          Div(),
+          CounterText(_count.toString()),
+          FloatingActionButton(_increment),
+        ],
       ),
-      children: [
-        Div(),
-        CounterText(_count.toString()),
-        FloatingActionButton(_increment),
-      ],
-    );
+    ];
   }
 }
 
-class CounterText extends Widget {
+class CounterText extends Component {
   CounterText(this.counter);
 
   final String counter;
 
   @override
-  Widget build() {
-    return Div(
-      props: DivProps(
-        styles: CssStyle(
-          width: Units.vw(100),
-        ).merge(DisplayFlex.column).merge(DisplayFlex.center),
-      ),
-      children: [
-        Span(
-          'You have pushed the button this many times:',
-          props: SpanProps(
-            styles: CssStyle(
-              color: 'black',
-              fontSize: Units.rem(1.5),
+  List<Component> build() {
+    return [
+      Div(
+        props: DivProps(
+          styles: CssStyle(
+            width: Units.vw(100),
+          ).merge(DisplayFlex.column).merge(DisplayFlex.center),
+        ),
+        children: [
+          Span(
+            'You have pushed the button this many times:',
+            props: SpanProps(
+              styles: CssStyle(
+                color: 'black',
+                fontSize: Units.rem(1.5),
+              ),
             ),
           ),
-        ),
-        Span(
-          counter,
-          props: SpanProps(
-            styles: CssStyle(
-              color: 'black',
-              fontSize: Units.rem(2),
+          Span(
+            counter,
+            props: SpanProps(
+              styles: CssStyle(
+                color: 'black',
+                fontSize: Units.rem(2),
+              ),
             ),
           ),
-        ),
-      ],
-    );
+        ],
+      )
+    ];
   }
 }
 
-class FloatingActionButton extends Widget {
+class FloatingActionButton extends Component {
   FloatingActionButton(this.onClick);
 
   final void Function() onClick;
 
   @override
-  Widget build() {
-    return Button(
-      props: ButtonProps(
-        innerText: '+',
-        onClick: (_) => onClick(),
-        styles: CssStyle(
-          width: Units.rem(5),
-          height: Units.rem(5),
-          margin: const Margin.all(40),
-          borderRadius: const BorderRadius.all(50),
-          backgroundColor: '#2196F3',
-          borderWidth: Units.none,
-          color: 'white',
-          fontSize: Units.rem(4),
-          boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.5)',
-          cursor: Cursor.pointer,
-        ).merge(DisplayFlex.center),
+  List<Component> build() {
+    return [
+      Button(
+        props: ButtonProps(
+          innerText: '+',
+          onClick: (_) => onClick(),
+          styles: CssStyle(
+            width: Units.rem(5),
+            height: Units.rem(5),
+            margin: const Margin.all(40),
+            borderRadius: const BorderRadius.all(50),
+            backgroundColor: '#2196F3',
+            borderWidth: Units.none,
+            color: 'white',
+            fontSize: Units.rem(4),
+            boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.5)',
+            cursor: Cursor.pointer,
+          ).merge(DisplayFlex.center),
+        ),
       ),
-    );
+    ];
   }
 }
